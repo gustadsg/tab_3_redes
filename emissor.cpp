790480b5b9d63650967baf59d94ce86096cd0124
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2022-02-14 08:59:17 -0300

    feat: recebe mensagem como texto

--- emissor.cpp
+++ emissor.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <sstream>
 #include "common.h"
@@ -16,6 +17,98 @@
 
 #define BUFSZ 500
 
+struct header mountHeader(std::string input, int issuerID, int lastMessageOrder)
+{
+	struct header header;
+
+	transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+	header.msgOrigin = issuerID;
+
+	if (!(input.rfind("planet", 0) == 0 || input.rfind("planetlist", 0) == 0))
+		header.msgOrder = lastMessageOrder++;
+
+	// hi
+	if (input.rfind("hi", 0) == 0)
+	{
+		header.msgType = 3;
+		header.msgOrder = 1;
+
+		return header;
+	}
+
+	// kill
+	if (input.rfind("kill", 0) == 0)
+	{
+		header.msgType = 4;
+
+		return header;
+	}
+
+	// msg
+	if (input.rfind("msg", 0) == 0)
+	{
+		header.msgType = 5;
+
+		return header;
+	}
+
+	// creq
+	if (input.rfind("creq", 0) == 0)
+	{
+		header.msgType = 6;
+
+		return header;
+	}
+
+	// origin
+	if (input.rfind("origin", 0) == 0)
+	{
+		header.msgType = 8;
+
+		return header;
+	}
+
+	// planet
+	if (input.rfind("planet", 0) == 0)
+	{
+		header.msgType = 9;
+		header.msgOrder = lastMessageOrder;
+
+		return header;
+	}
+
+	// planetlist
+	if (input.rfind("planetlist", 0) == 0)
+	{
+		header.msgType = 10;
+		header.msgOrder = lastMessageOrder;
+
+		return header;
+	}
+}
+
+std::vector<std::string> split(std::string str, char delimiter)
+{
+	std::vector<std::string> internal;
+	int start = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == delimiter)
+		{
+			internal.push_back(str.substr(start, i - start));
+			start = i + 1;
+		}
+		if (i == str.length() - 1)
+		{
+			internal.push_back(str.substr(start, i - start + 1));
+		}
+	}
+
+	return internal;
+}
+
 int main(int argc, char **argv)
 {
 	char str[14];
@@ -23,7 +116,7 @@ int main(int argc, char **argv)
 	char *ip = strtok(str, ":");								 // gets IP
 	char *port = strtok(NULL, ":");							 // gets port
 	std::string planetName = randomPlanetName(); // gets planet name
-	std::cout << "Planet name: " << planetName << std::endl;
+	std::string input;
 
 	if (argv[2] < 0)
 	{
@@ -74,6 +167,7 @@ int main(int argc, char **argv)
 	if (issuerHeader.msgOrder == 0)
 	{
 		// sends an "HI" message type (3)
+		std::cout << "> hi" << std::endl;
 		issuerHeader.msgType = 3;
 		count = send(sock, &issuerHeader, sizeof(header), 0);
 
@@ -83,23 +177,25 @@ int main(int argc, char **argv)
 		}
 
 		count = recv(sock, &issuerHeader, sizeof(header), 0);
+		issuerID = issuerHeader.msgDestiny;
 
-		issuerID = issuerHeader.msgDestiny; // this issuer ID
+		std::cout << "< ok " << issuerID << std::endl;
 	}
 
 	if (issuerHeader.msgType == 1)
 	{
-		std::cout << "\nissuer connected - ID:" << issuerID << std::endl;
 		// inform to server the origin planet
 		memset(buf, 0, BUFSZ);
 		std::ostringstream msg;
 		msg << "origin " << planetName.length() << " " << planetName.c_str() << std::endl;
 		memcpy(buf, msg.str().c_str(), msg.str().length());
+		std::cout << "> " << msg.str();
 
 		unsigned short size_planet = strlen(buf);
 		issuerHeader.msgType = 8;
 		issuerHeader.msgDestiny = exhibitorID;
 		issuerHeader.msgOrigin = issuerID;
+		issuerHeader.msgOrder += 1;
 		count = send(sock, &issuerHeader, sizeof(header), 0);
 		count = send(sock, &size_planet, sizeof(size_planet), 0);
 		count = send(sock, buf, size_planet, 0);
@@ -107,16 +203,16 @@ int main(int argc, char **argv)
 
 		if (issuerHeader.msgType == 1)
 		{
-			std::cout << "origin message delivered!" << std::endl;
+			std::cout << "< ok" << std::endl;
 		}
 
 		// connection accepted
 		while (1)
 		{
-
-			issuerHeader.msgOrder++;
-			issuerHeader.msgOrigin = issuerID;
-			issuerHeader.msgType = getsType();
+			std::cout << "> ";
+			getline(std::cin, input, '\n');
+			std::vector<std::string> args = split(input, ' ');
+			issuerHeader = mountHeader(input, issuerID, issuerHeader.msgOrder);
 
 			if (issuerHeader.msgType == 4)
 			{
@@ -129,64 +225,55 @@ int main(int argc, char **argv)
 			else
 			{
 				if (issuerHeader.msgType != 9)
-					issuerHeader.msgDestiny = getsDestiny(exhibitorID);
+					issuerHeader.msgDestiny = atoi(args[1].c_str());
 
 				switch (issuerHeader.msgType)
 				{
-				case 5:
+				case 5: // msg idPlanet numCharsMsg message
 				{
+					int startOfMessage = args[0].length() + args[1].length() + args[2].length() + 3;
+					std::string message = input.substr(startOfMessage);
 					count = send(sock, &issuerHeader, sizeof(header), 0);
-
-					std::cout << "\n> send message to " << issuerHeader.msgDestiny << ": " << std::endl;
-					memset(buf, 0, BUFSZ);
-					std::cin.ignore();
-					std::cin.getline(buf, BUFSZ);
-
-					unsigned short size = strlen(buf);
-					count = send(sock, &size, sizeof(size), 0); // sends message's size first
-					count = send(sock, buf, size, 0);						// sends message
+					unsigned short size = atoi(args[2].c_str());
+					count = send(sock, &size, sizeof(size), 0);		// sends message's size first
+					count = send(sock, message.c_str(), size, 0); // sends message
 
 					count = recv(sock, &issuerHeader, sizeof(header), 0); // receives an "OK" message
 
 					if (issuerHeader.msgType == 1)
 					{
-						std::cout << "\n message delivered!" << std::endl;
+						std::cout << "< ok" << std::endl;
 					}
 
 					issuerHeader.msgOrder++;
 				}
 				break;
-				case 6:
+				case 6: // creq idPlanet
 					count = send(sock, &issuerHeader, sizeof(header), 0);
 
 					recv(sock, &issuerHeader, sizeof(header), 0); // receives an "OK" message
 					if (issuerHeader.msgType == 1)
 					{
-						std::cout << "\n message delivered!" << std::endl;
+						std::cout << "< ok" << std::endl;
 					}
 
 					issuerHeader.msgOrder++;
 					break;
-				case 9:
-					memset(buf, 0, BUFSZ);
-					std::cout << "type the client you want to know the planet: ";
-					std::cin.ignore();
-					cin.getline(buf, BUFSZ);
-					issuerHeader.msgDestiny = atoi(buf);
-
+				case 9: // planet idToFind
+					issuerHeader.msgDestiny = atoi(args[1].c_str());
 					count = send(sock, &issuerHeader, sizeof(header), 0);
 					recv(sock, &issuerHeader, sizeof(header), 0); // receives an "OK" message
 					if (issuerHeader.msgType == 1)
 					{
-						std::cout << "\n message delivered!" << std::endl;
+						std::cout << "< ok" << std::endl;
 					}
 					break;
-				case 10:
+				case 10: // planet idToFind
 					count = send(sock, &issuerHeader, sizeof(header), 0);
 					recv(sock, &issuerHeader, sizeof(header), 0); // receives an "OK" message
 					if (issuerHeader.msgType == 1)
 					{
-						std::cout << "\n message delivered!" << std::endl;
+						std::cout << "< ok" << std::endl;
 					}
 					break;
 				default:
